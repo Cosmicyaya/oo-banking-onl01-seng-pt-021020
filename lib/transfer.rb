fb3899dc6c863808a57185399d4221ca4ba83eb7
Ryan Hollins <ballbaroi@gmail.com>
2020-03-16 15:35:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,11 @@ class Transfer
     @sender.balance -= @amount
     @receiver.balance += @amount
     @status = "complete"
-    
+  else
+    @status = "rejected"
+    return "Transaction rejected. Please check your account balance."
+  end
+end
     def reverse_transfer
       if @status == "complete"
         @sender.balance += @amount
