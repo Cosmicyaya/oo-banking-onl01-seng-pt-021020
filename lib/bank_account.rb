f95e067ff37496d157a8acc422b16879bec2c9bc
Ryan Hollins <ballbaroi@gmail.com>
2020-03-16 14:54:29 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,10 @@
 class BankAccount
-  attr_accessor :balance
+  attr_accessor :balance, :status
   attr_reader :name
   
   def initialize(bank_account)
     @name = bank_account
     @balance = 1000
+    @status = "open"
   end
 end
